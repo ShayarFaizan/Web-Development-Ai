c8650f065c5b4f2a8c9122f307ea46c31ba585f6
Faizan Ali Rizvi <shayarfaizan786@gmail.com>
2026-03-27 19:21:29 +0530

    Add Google Tag Manager (GTM) scripts to layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -75,6 +75,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TSP7SDC8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {/* Google Analytics */}
         <Script
           strategy="afterInteractive"
@@ -92,6 +102,22 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Google Tag Manager (script) */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TSP7SDC8');
+            `,
+          }}
+        />
+
         <AppProvider>
           <GoogleOneTap />
           <Navbar />
